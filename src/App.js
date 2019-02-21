01bf5f89ee37b84e256df86cb0b7d262f919b9bc
Tony DeCarlo <anthony.decarlo@t1cg.com>
2019-02-21 15:13:01 -0500

    changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Testing feature branch
           </a>
         </header>
       </div>
